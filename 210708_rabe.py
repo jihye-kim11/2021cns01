2416d727ee8da12aeb382f24e97db7457877e2fa
maybethiswilldo <cks0229@hanmail.net>
2021-07-08 21:58:59 +0900

    minor fixes

--- 210708_rabe.py
+++ 210708_rabe.py
@@ -1,14 +1,45 @@
 import wiotp.sdk.device
 import serial
+import sqlite3
+import datetime as dt
 
-def myCommandCallback(cmd): ## 
+
+def myCommandCallback(cmd):
     print("Command : %s" % cmd.data)
-    if cmd.commandId == "door" :
-        if cmd.data.get("state") == "run":
-            ser.write(b"run")
-        if cmd.data.get("state") == "stop":
-            ser.write(b"stop")
-            
+    if cmd.commandId == "robot":
+        if cmd.data.get("state") == "start":
+            ser.write(b"start")
+            global starttime, box_id
+            box_id += 1
+            starttime = cmd.timestamp.strftime('%H:%M:%S')
+            cur.execute('insert into transport_data values (?, ?, ?, ?)', (box_id, colortype, starttime, endtime))
+            conn.commit()
+        if cmd.data.get("state") == "back":
+            ser.write(b"back")
+
+    if cmd.commandId == "update":
+        if cmd.data.get("state") == "request":
+            client.publishEvent(eventId="transportion", msgFormat="json", data=data_js, qos=0, onPublish=None)
+
+
+def colorcount(cotype):
+    global yellow, green, blue, total
+
+    if cotype == 'yellow':
+        yellow += 1
+        total += 1
+
+    elif cotype == 'green':
+        green += 1
+        total += 1
+
+    elif cotype == 'blue':
+        blue += 1
+        total += 1
+
+    return [total,yellow,green,blue]
+
+
 myConfig = { 
     "identity": {
         "orgId": "wf2ir5",
@@ -19,6 +50,12 @@ myConfig = {
         "token": "dlxorud772"
     }
 }
+
+# DB 생성
+conn = sqlite3.connect("transport.db")
+cur = conn.cursor()
+conn.execute('CREATE TABLE transport_data(id INTEGER PRIMARY KEY, colortype TEXT, starttime TEXT, endtime TEXT)')
+
 client = wiotp.sdk.device.DeviceClient(config=myConfig)
 client.commandCallback = myCommandCallback
 
@@ -28,53 +65,31 @@ client.connect()
 ser = serial.Serial('/dev/rfcomm0')
 
 
+starttime, box_id, endtime, total, yellow, green, blue = '', 0, '', 0, 0, 0, 0
 
-total = 0
-yellow = 0
-green = 0
-blue = 0
-def colorcount(cotype):
-    global yellow
-    global green
-    global blue
-    global total
-
-    if cotype == 'yellow':
-        yellow += 1
-        total += 1
-
-    elif cotype == 'green':
-        yellow += 1
-        total += 1
-
-    elif cotype == 'blue':
-        yellow += 1
-        total += 1
-
-    return [total,yellow,green,blue]
-        
 # Send Data
 while True:
     msg = ser.readline()
     text = msg.decode()
-    text = text.split(',',2)
-    print('text: ',text)
+    text = text.split(',', 2)
+    print('text: ', text)
 
     flag = text[0]
-    # if flag == 'startcomplete':
+    # if flag == 'trans_start':
     #     colortype = text[1].split('\n')[0]
     #     data_js = {flag : colortype}
-        
     #     print('data_js: ',data_js)
     #     client.publishEvent(eventId="transportion", msgFormat="json", data=data_js, qos=0, onPublish=None)
     
     if flag == 'trans_complete': # 물류 배송이 다 끝나면 cloud로 PUBLISH
         colortype = text[1].split('\n')[0]
         result_list = colorcount(colortype)
-        data_js = {"total" : result_list[0], "yellow" : result_list[1], "green" : result_list[2],"blue" : result_list[3]} 
-        print('data_js: ',data_js)
-
+        endtime = dt.datetime.now().strftime('%H:%M:%S')
+        cur.execute('UPDATE transport_data SET endtime = ? WHERE id = ?', (endtime, box_id))
+        data_js = {"total": result_list[0], "yellow": result_list[1], "green": result_list[2], "blue": result_list[3],
+                   "id": box_id, "colortype": colortype, "starttime": starttime, "endtime": endtime}
+        print('data_js: ', data_js)
         client.publishEvent(eventId="transportion", msgFormat="json", data=data_js, qos=0, onPublish=None)
-    
+
 # Disconnect
 client.disconnect()
